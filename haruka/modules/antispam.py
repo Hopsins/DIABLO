2011349288eaa7fd8bfbc1d1d793fe4d03785aaa
AvatarX0TIC2 <51491592+AvatarX0TIC2@users.noreply.github.com>
2019-06-20 18:42:38 +0530

    Update antispam.py

--- haruka/modules/antispam.py
+++ haruka/modules/antispam.py
@@ -96,7 +96,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
     ok123 = mention_html(user_chat.id, user_chat.first_name)
 
 
-    text12 = f"Summoning the ban axe on Chu {ok123} 😉."
+    text12 = f"Globally ban started on {ok123} 😉."
     update.effective_message.reply_text(text12, parse_mode=ParseMode.HTML)
 
     banner = update.effective_user  # type: Optional[User]
@@ -138,7 +138,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
                    "{} has been successfully gbanned!".format(mention_html(user_chat.id, user_chat.first_name)),
                    html=True)
-    text13 = f"Chu {ok123} has been Dealt with 😈."
+    text13 = f"Aha {ok123} has been globally banned 😈."
     update.effective_message.reply_text(text13, parse_mode=ParseMode.HTML)
 
 
