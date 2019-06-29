538dff0228a2c09560de7530312617a77b9d287a
AvatarX0TIC2 <51491592+AvatarX0TIC2@users.noreply.github.com>
2019-06-30 03:16:26 +0530

    Update welcome.py

--- haruka/modules/welcome.py
+++ haruka/modules/welcome.py
@@ -95,8 +95,8 @@ def new_member(bot: Bot, update: Update):
                 continue
                 
                 # Give the sudo a special welcome
-            if new_mem.id == 839894103:
-                bot.send_message(chat.id, "Yo {} Sudo user is here,welcome")
+            if new_mem.id == 90296554:
+                bot.send_message(chat.id, "Yo,@ABoiHasNoName welcome my  Sudo  is here")
                 continue
 
             # Give start information when add bot to group
