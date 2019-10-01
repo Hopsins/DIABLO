be24ec1eaba0ba64176e632b942c4298b2f950bc
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-01 11:51:21 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -116,6 +116,324 @@ def get_id(bot: Bot, update: Update, args: List[str]):
                                                 parse_mode=ParseMode.MARKDOWN)
 
 
+import subprocess
+import html
+import json
+import random
+import time
+import pyowm
+import wikipedia
+import re
+from pyowm import timeutils, exceptions
+from datetime import datetime
+from typing import Optional, List
+from pythonping import ping as ping3
+from typing import Optional, List
+from PyLyrics import *
+from hurry.filesize import size
+
+import requests
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from telegram import ParseMode, ReplyKeyboardRemove, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import CommandHandler, run_async, Filters
+from telegram.utils.helpers import escape_markdown, mention_html
+from telegram.error import BadRequest
+
+from haruka import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS, BAN_STICKER
+from haruka.__main__ import GDPR
+from haruka.__main__ import STATS, USER_INFO
+from haruka.modules.disable import DisableAbleCommandHandler
+from haruka.modules.helper_funcs.extraction import extract_user
+from haruka.modules.helper_funcs.filters import CustomFilters
+from haruka.modules.rextester.api import Rextester, CompilerError
+from haruka.modules.rextester.langs import languages
+
+from haruka.modules.sql.translation import prev_locale
+
+from haruka.modules.translations.strings import tld
+
+from requests import get
+
+
+@run_async
+def insults(bot: Bot, update: Update):
+    chat = update.effective_chat  # type: Optional[Chat]
+    text = random.choice(tld(chat.id, "INSULTS-K"))
+    update.effective_message.reply_text(text)
+
+
+@run_async
+def runs(bot: Bot, update: Update):
+    chat = update.effective_chat  # type: Optional[Chat]
+    update.effective_message.reply_text(random.choice(tld(chat.id, "RUNS-K")))
+
+
+@run_async
+def slap(bot: Bot, update: Update, args: List[str]):
+    chat = update.effective_chat  # type: Optional[Chat]
+    msg = update.effective_message  # type: Optional[Message]
+
+    # reply to correct message
+    reply_text = msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
+
+    # get user who sent message
+    if msg.from_user.username:
+        curr_user = "@" + escape_markdown(msg.from_user.username)
+    else:
+        curr_user = "[{}](tg://user?id={})".format(msg.from_user.first_name, msg.from_user.id)
+
+    user_id = extract_user(update.effective_message, args)
+    if user_id:
+        slapped_user = bot.get_chat(user_id)
+        user1 = curr_user
+        if slapped_user.username:
+            user2 = "@" + escape_markdown(slapped_user.username)
+        else:
+            user2 = "[{}](tg://user?id={})".format(slapped_user.first_name,
+                                                   slapped_user.id)
+
+    # if no target found, bot targets the sender
+    else:
+        user1 = "[{}](tg://user?id={})".format(bot.first_name, bot.id)
+        user2 = curr_user
+
+    temp = random.choice(tld(chat.id, "SLAP_TEMPLATES-K"))
+    item = random.choice(tld(chat.id, "ITEMS-K"))
+    hit = random.choice(tld(chat.id, "HIT-K"))
+    throw = random.choice(tld(chat.id, "THROW-K"))
+    itemp = random.choice(tld(chat.id, "ITEMP-K"))
+    itemr = random.choice(tld(chat.id, "ITEMR-K"))
+
+    repl = temp.format(user1=user1, user2=user2, item=item, hits=hit, throws=throw, itemp=itemp, itemr=itemr)
+    #user1=user1, user2=user2, item=item_ru, hits=hit_ru, throws=throw_ru, itemp=itemp_ru, itemr=itemr_ru
+
+    reply_text(repl, parse_mode=ParseMode.MARKDOWN)
+
+
+@run_async
+def get_bot_ip(bot: Bot, update: Update):
+    """ Sends the bot's IP address, so as to be able to ssh in if necessary.
+        OWNER ONLY.
+    """
+    res = requests.get("http://ipinfo.io/ip")
+    update.message.reply_text(res.text)
+
+
+@run_async
+def get_id(bot: Bot, update: Update, args: List[str]):
+    user_id = extract_user(update.effective_message, args)
+    chat = update.effective_chat  # type: Optional[Chat]
+    if user_id:
+        if update.effective_message.reply_to_message and update.effective_message.reply_to_message.forward_from:
+            user1 = update.effective_message.reply_to_message.from_user
+            user2 = update.effective_message.reply_to_message.forward_from
+            update.effective_message.reply_text(tld(chat.id,
+                "The original sender, {}, has an ID of `{}`.\nThe forwarder, {}, has an ID of `{}`.").format(
+                    escape_markdown(user2.first_name),
+                    user2.id,
+                    escape_markdown(user1.first_name),
+                    user1.id),
+                parse_mode=ParseMode.MARKDOWN)
+        else:
+            user = bot.get_chat(user_id)
+            update.effective_message.reply_text(tld(chat.id, "{}'s id is `{}`.").format(escape_markdown(user.first_name), user.id),
+                                                parse_mode=ParseMode.MARKDOWN)
+    else:
+        chat = update.effective_chat  # type: Optional[Chat]
+        if chat.type == "private":
+            update.effective_message.reply_text(tld(chat.id, "Your id is `{}`.").format(chat.id),
+                                                parse_mode=ParseMode.MARKDOWN)
+
+        else:
+            update.effective_message.reply_text(tld(chat.id, "This group's id is `{}`.").format(chat.id),
+                                                parse_mode=ParseMode.MARKDOWN)
+
+
+@run_async
+def info(bot: Bot, update: Update, args: List[str]):
+    msg = update.effective_message  # type: Optional[Message]
+    user_id = extract_user(update.effective_message, args)
+    chat = update.effective_chat  # type: Optional[Chat]
+
+    if user_id:
+        user = bot.get_chat(user_id)
+
+    elif not msg.reply_to_message and not args:
+        user = msg.from_user
+
+    elif not msg.reply_to_message and (not args or (
+            len(args) >= 1 and not args[0].startswith("@") and not args[0].isdigit() and not msg.parse_entities(
+        [MessageEntity.TEXT_MENTION]))):
+        msg.reply_text(tld(chat.id, "I can't extract a user from this."))
+        return
+
+    else:
+        return
+
+    text =  tld(chat.id, "<b>User info</b>:")
+    text += "\nID: <code>{}</code>".format(user.id)
+    text += tld(chat.id, "\nFirst Name: {}").format(html.escape(user.first_name))
+
+    if user.last_name:
+        text += tld(chat.id, "\nLast Name: {}").format(html.escape(user.last_name))
+
+    if user.username:
+        text += tld(chat.id, "\nUsername: @{}").format(html.escape(user.username))
+
+    text += tld(chat.id, "\nUser link: {}\n").format(mention_html(user.id, "link"))
+
+    if user.id == OWNER_ID:
+        text += tld(chat.id, "\n\nAy, This guy is my owner. I would never do anything against him!")
+    else:
+        if user.id in SUDO_USERS:
+            text += tld(chat.id, "\nThis person is one of my sudo users! " \
+            "Nearly as powerful as my owner - so watch it.")
+        else:
+            if user.id in SUPPORT_USERS:
+                text += tld(chat.id, "\nThis person is one of my support users! " \
+                        "Not quite a sudo user, but can still gban you off the map.")
+
+            if user.id in WHITELIST_USERS:
+                text += tld(chat.id, "\nThis person has been whitelisted! " \
+                        "That means I'm not allowed to ban/kick them.")
+
+    for mod in USER_INFO:
+        mod_info = mod.__user_info__(user.id, chat.id).strip()
+        if mod_info:
+            text += "\n\n" + mod_info
+
+    update.effective_message.reply_text(text, parse_mode=ParseMode.HTML)
+
+
+@run_async
+def echo(bot: Bot, update: Update):
+    message = update.effective_message
+    message.delete()
+    args = update.effective_message.text.split(None, 1)
+    if message.reply_to_message:
+        message.reply_to_message.reply_text(args[1])
+    else:
+        message.reply_text(args[1], quote=False)
+
+
+@run_async
+def reply_keyboard_remove(bot: Bot, update: Update):
+    reply_keyboard = []
+    reply_keyboard.append([
+        ReplyKeyboardRemove(
+            remove_keyboard=True
+        )
+    ])
+    reply_markup = ReplyKeyboardRemove(
+        remove_keyboard=True
+    )
+    old_message = bot.send_message(
+        chat_id=update.message.chat_id,
+        text='trying',
+        reply_markup=reply_markup,
+        reply_to_message_id=update.message.message_id
+    )
+    bot.delete_message(
+        chat_id=update.message.chat_id,
+        message_id=old_message.message_id
+    )
+
+
+@run_async
+def gdpr(bot: Bot, update: Update):
+    update.effective_message.reply_text(tld(update.effective_chat.id, "Deleting identifiable data..."))
+    for mod in GDPR:
+        mod.__gdpr__(update.effective_user.id)
+
+    update.effective_message.reply_text(tld(update.effective_chat.id, "send_gdpr"), parse_mode=ParseMode.MARKDOWN)
+
+
+@run_async
+def markdown_help(bot: Bot, update: Update):
+    chat = update.effective_chat  # type: Optional[Chat]
+    update.effective_message.reply_text(tld(chat.id, "MARKDOWN_HELP-K"), parse_mode=ParseMode.HTML)
+    update.effective_message.reply_text(tld(chat.id, "Try forwarding the following message to me, and you'll see!"))
+    update.effective_message.reply_text(tld(chat.id, "/save test This is a markdown test. _italics_, *bold*, `code`, "
+                                        "[URL](example.com) [button](buttonurl:github.com) "
+                                        "[button2](buttonurl://google.com:same)"))
+
+
+@run_async
+def stats(bot: Bot, update: Update):
+    update.effective_message.reply_text("Current stats:\n" + "\n".join([mod.__stats__() for mod in STATS]))
+
+
+def ping(bot: Bot, update: Update):
+    tg_api = ping3('api.telegram.org', count=4)
+    google = ping3('google.com', count=4)
+    print(google)
+    text = "*Pong!*\n"
+    text += "Average speed to Telegram bot API server - `{}` ms\n".format(tg_api.rtt_avg_ms)
+    if google.rtt_avg:
+        gspeed = google.rtt_avg
+    else:
+        gspeed = google.rtt_avg
+    text += "Average speed to Google - `{}` ms".format(gspeed)
+    update.effective_message.reply_text(text, parse_mode=ParseMode.MARKDOWN)
+
+#def google(bot: Bot, update: Update):
+#        query = update.effective_message.text.split(" ",1)
+#        result_ = subprocess.run(['gsearch', str(query[1])], stdout=subprocess.PIPE)
+#        result = str(result_.stdout.decode())
+#        update.effective_message.reply_text('*Searching:*\n`' + str(query[1]) + '`\n\n*RESULTS:*\n' + result, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+
+@run_async
+def github(bot: Bot, update: Update):
+    message = update.effective_message
+    text = message.text[len('/git '):]
+    usr = get(f'https://api.github.com/users/{text}').json()
+    if usr.get('login'):
+        text = f"*Username:* [{usr['login']}](https://github.com/{usr['login']})"
+
+        whitelist = ['name', 'id', 'type', 'location', 'blog',
+                     'bio', 'followers', 'following', 'hireable',
+                     'public_gists', 'public_repos', 'email',
+                     'company', 'updated_at', 'created_at']
+
+        difnames = {'id': 'Account ID', 'type': 'Account type', 'created_at': 'Account created at',
+                    'updated_at': 'Last updated', 'public_repos': 'Public Repos', 'public_gists': 'Public Gists'}
+
+        goaway = [None, 0, 'null', '']
+
+        for x, y in usr.items():
+            if x in whitelist:
+                if x in difnames:
+                    x = difnames[x]
+                else:
+                    x = x.title()
+
+                if x == 'Account created at' or x == 'Last updated':
+                    y = datetime.strptime(y, "%Y-%m-%dT%H:%M:%SZ")
+
+                if y not in goaway:
+                    if x == 'Blog':
+                        x = "Website"
+                        y = f"[Here!]({y})"
+                        text += ("\n*{}:* {}".format(x, y))
+                    else:
+                        text += ("\n*{}:* `{}`".format(x, y))
+        reply_text = text
+    else:
+        reply_text = "User not found. Make sure you entered valid username!"
+    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+            
+    
+    def repo(bot: Bot, update: Update, args: List[str]):
+    message = update.effective_message
+    text = message.text[len('/repo '):]
+    usr = get(f'https://api.github.com/users/{text}/repos?per_page=40').json()
+    reply_text = "*Repo*\n"
+    for i in range(len(usr)):
+        reply_text += f"[{usr[i]['name']}]({usr[i]['html_url']})\n"
+    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+    
 @run_async
 def info(bot: Bot, update: Update, args: List[str]):
     msg = update.effective_message  # type: Optional[Message]
@@ -361,6 +679,8 @@ __help__ = """
  - /time <place>: gives the local time at the given place.
  - /weather <city>: get weather info in a particular place.
  - /info: get information about a user.
+ - /git: Returns info about a GitHub user or organization.
+ - /repo: Return the GitHub user or organization repository list (Limited at 40)
  - /gdpr: deletes your information from the bot's database. Private chats only.
  - /stickerid: reply to a sticker to me to tell you its file ID.
  - /getsticker: reply to a sticker to me to upload its raw PNG file.
@@ -377,6 +697,8 @@ TIME_HANDLER = DisableAbleCommandHandler("time", get_time, pass_args=True)
 
 RUNS_HANDLER = DisableAbleCommandHandler("runs", runs)
 SLAP_HANDLER = DisableAbleCommandHandler("slap", slap, pass_args=True)
+GITHUB_HANDLER = DisableAbleCommandHandler("git", github, admin_ok=True)
+REPO_HANDLER = DisableAbleCommandHandler("repo", repo, pass_args=True, admin_ok=True)
 INFO_HANDLER = DisableAbleCommandHandler("info", info, pass_args=True)
 
 ECHO_HANDLER = CommandHandler("echo", echo, filters=Filters.user(OWNER_ID))
@@ -394,6 +716,8 @@ dispatcher.add_handler(IP_HANDLER)
 dispatcher.add_handler(TIME_HANDLER)
 dispatcher.add_handler(RUNS_HANDLER)
 dispatcher.add_handler(SLAP_HANDLER)
+dispatcher.add_handler(GITHUB_HANDLER)
+dispatcher.add_handler(REPO_HANDLER)
 dispatcher.add_handler(INFO_HANDLER)
 dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
