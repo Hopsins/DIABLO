7fcbf4e4e9c3402db06315c998b9a75dd66001d2
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-01 09:32:08 +0530

    Update global_bans.py

--- haruka/modules/global_bans.py
+++ haruka/modules/global_bans.py
@@ -7,7 +7,7 @@ from telegram.error import BadRequest, TelegramError
 from telegram.ext import run_async, CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import mention_html
 
-import IHbot.modules.sql.global_bans_sql as sql
+import haruka.modules.sql.global_bans_sql as sql
 from haruka import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, STRICT_GBAN
 from haruka.modules.helper_funcs.chat_status import user_admin, is_user_admin
 from haruka.modules.helper_funcs.extraction import extract_user, extract_user_and_text
