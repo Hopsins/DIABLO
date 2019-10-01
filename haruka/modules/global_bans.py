de082848e5dfa584a7581aad9e28ce02846d52f6
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-01 09:49:46 +0530

    Update global_bans.py

--- haruka/modules/global_bans.py
+++ haruka/modules/global_bans.py
@@ -91,7 +91,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
 
         return
 
-    message.reply_text("*Summoning the ban axe* 😉")
+    message.reply_text("*Globally Ban Initiated 😉")
 
     banner = update.effective_user  # type: Optional[User]
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
@@ -132,7 +132,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
                    "{} has been successfully gbanned!".format(mention_html(user_chat.id, user_chat.first_name)),
                    html=True)
-    message.reply_text("Person has been \"Dealt with\".")
+    message.reply_text("Person has been \"Globally Banned\".")
 
 @run_async
 def ungban(bot: Bot, update: Update, args: List[str]):
